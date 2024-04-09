d90ab3e05e69a7b40236d22904ee0c904df38c43
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2024-04-09 17:53:11 +0000

    Modify portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ashhad Waquas Syed",
+  title: "AI Enthusiast",
+  email: "ashhadwsyed@gmail.com",
+  gitHub: "waquas9248",
+  linkedIn: "ashhad-waquas-syed",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
